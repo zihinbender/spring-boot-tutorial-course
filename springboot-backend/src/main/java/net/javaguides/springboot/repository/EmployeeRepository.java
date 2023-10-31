3da76e76106345353b6aed106c6177570890e151
budak <budakaziz@gmail.com>
2023-10-31 02:03:19 +0200

    Deleted for simplicity

--- springboot-backend/src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package net.javaguides.springboot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import net.javaguides.springboot.model.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long>{
-
-}
