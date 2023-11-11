75045d5eb48c2f7af1e679a42dc3d2818d3f5546
budak <budakaziz@gmail.com>
2023-11-11 17:19:05 +0300

    first commit

--- student-management-system/src/main/java/net/javaguides/sms/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package net.javaguides.sms.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import net.javaguides.sms.entity.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Long>{
-
-}
